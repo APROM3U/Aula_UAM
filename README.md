6b7404604521df473fda418cd42e42835937f088
Alexandre <pradoromeu@hotmail.com>
2021-08-25 20:57:21 -0300

    Atividades realizadas em aula

---
+++ README.md
@@ -0,0 +1,7 @@
+# UAM - Desenvolvimento de Software para Web
+## Aulas utilizando 
+
+* PHP - back-end
+* HTML/CSS/JavaScript - front-end
+* Framework Bootstrap
+* MySQL - Database
---
+++ php/cadastro.php
@@ -0,0 +1,10 @@
+<?php
+    // Sempre que for pegar a variável utilizar $
+    // o ponto é o concatenador
+    echo "Nome: ".$_POST['nome'];
+    echo "Data de nascimento: ".$_POST['nasc'];
+    echo "E-mail: ".$_POST['email'];
+    echo "Sexo: ".$_POST['genero'];
+    echo "Informações adicionais: ".$_POST['info'];
+
+?>
